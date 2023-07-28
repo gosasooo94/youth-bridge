d3f4e8888511cf4ad4dc03ef3ae7329e522efa8b
04-13 <04-13@DESKTOP-ULJVJG3>
2023-07-28 17:54:02 +0900

    test`111

---
+++ care/src/main/java/com/javaclass/domain/FairVO2.java
@@ -0,0 +1,41 @@
+package com.javaclass.domain;
+
+public class FairVO2 {
+	private String fairName;
+	private String fairAddr;
+	private String fairStart;
+	private String fairEnd;
+	private String fairCate;
+	
+	public String getFairName() {
+		return fairName;
+	}
+	public void setFairName(String fairName) {
+		this.fairName = fairName;
+	}
+	public String getFairAddr() {
+		return fairAddr;
+	}
+	public void setFairAddr(String fairAddr) {
+		this.fairAddr = fairAddr;
+	}
+	public String getFairStart() {
+		return fairStart;
+	}
+	public void setFairStart(String fairStart) {
+		this.fairStart = fairStart;
+	}
+	public String getFairEnd() {
+		return fairEnd;
+	}
+	public void setFairEnd(String fairEnd) {
+		this.fairEnd = fairEnd;
+	}
+	public String getFairCate() {
+		return fairCate;
+	}
+	public void setFairCate(String fairCate) {
+		this.fairCate = fairCate;
+	}
+	
+}
