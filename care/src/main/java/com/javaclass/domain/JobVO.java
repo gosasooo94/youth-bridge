24f8313eaf669d06a927959fdb3bdd34ca72c1e8
lee <lee@DESKTOP-364FDL9>
2023-07-28 17:15:34 +0900

    test

---
+++ care/src/main/java/com/javaclass/domain/JobVO.java
@@ -0,0 +1,43 @@
+package com.javaclass.domain;
+
+public class JobVO {
+	private String jobName;
+	private String jobAddr;
+	private String jobTel;
+	private String jobStart;
+	private String jobEnd;
+	
+	public String getJobName() {
+		return jobName;
+	}
+	public void setJobName(String jobName) {
+		this.jobName = jobName;
+	}
+	public String getJobAddr() {
+		return jobAddr;
+	}
+	public void setJobAddr(String jobAddr) {
+		this.jobAddr = jobAddr;
+	}
+	public String getJobTel() {
+		return jobTel;
+	}
+	public void setJobTel(String jobTel) {
+		this.jobTel = jobTel;
+	}
+	public String getJobStart() {
+		return jobStart;
+	}
+	public void setJobStart(String jobStart) {
+		this.jobStart = jobStart;
+	}
+	public String getJobEnd() {
+		return jobEnd;
+	}
+	public void setJobEnd(String jobEnd) {
+		this.jobEnd = jobEnd;
+	}
+	
+	
+	
+}
